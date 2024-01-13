e684728df7b468a0108b1dea7ae9393d219b62f9
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:20:02 +0300

    C libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncpy - empty function
+ * @dest: Parameter
+ * @src: Parameter
+ * @n: Parameter
+ * Return: returns empty
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+return (strncpy(dest, src, n));
+}
+
