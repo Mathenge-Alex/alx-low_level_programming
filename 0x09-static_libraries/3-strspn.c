28d88e3c674dcb77651b57b923560a6c28eb8abf
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:39:05 +0300

    C libraries

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strspn - empty function
+ * @s: Parameter
+ * @accept: Parameter
+ * Return: return
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+return (strspn(s, accept));
+}
+
