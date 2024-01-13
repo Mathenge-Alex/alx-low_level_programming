5a46c39ad1a3cc99a6aa70efa5fcbb89c978ac1e
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:05:35 +0300

    C libraries

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * *_memcpy - empty function
+ * @dest: Parameter
+ * @src: Parameter
+ * @n: Parameter
+ * Return: returns empty
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memcpy(dest, src, n));
+}
+
