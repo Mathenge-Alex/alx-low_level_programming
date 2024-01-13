12e75510187b4ab1c31e17e204303bba02b87431
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 04:03:38 +0300

    C libraries

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - function will write the character c to stdout
  * @c: Character to print
  *
- * return: 1 on success and
+ * Return: 1 on success and
  * -1 on error. Errno is set appropriately.
  */
 int _putchar(char c)
