d9646e31d650940cd03335217484bca4d34e8529
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 04:08:25 +0300

    C libraries

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_memset - empty function
+ * @s: Parameter
+ * @b: Parameter
+ * @n: Parameter
+ * Return: returns empty
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s, b, n));
+}
