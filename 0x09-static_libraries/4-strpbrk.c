5e781f8e6cbd3235d32fb4e3a0b12b467716f111
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:53:43 +0300

    C libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *_strpbrk - empty function
+ * @s: Parameter
+ * @accept: Parameter
+ * Return: return
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept));
+}
+
