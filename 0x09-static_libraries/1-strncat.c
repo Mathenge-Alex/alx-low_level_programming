dd91d29a88c900df03f26b6df0765994c30149e6
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:11:43 +0300

    C libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *_strncat - empty function
+ * @dest: Parameter
+ * @src: Parameter
+ * @n: Parameter
+ * Return: returns empty
+ */
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+return (strncat(dest, src, n));
+}
