07d4818145be9f63c9492ef360fc7fd080afb03c
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:57:53 +0300

    C libraries

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strcpy - empty function
+ * @dest: Parameter
+ * @src: Parameter
+ * Return: return
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
+
