6463a6e753028514d997f1faf9a0c31f4c55b867
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 05:55:27 +0300

    C libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strstr - empty function
+ * @haystack: Parameter
+ * @needle: Parameter
+ * Return: return
+ */
+
+
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}
