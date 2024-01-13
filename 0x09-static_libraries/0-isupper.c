d96ff1d9d875ba336462fa0c31ab42a6c1c3e25e
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 04:03:56 +0300

    C libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - function checks if super
+ * @c: Character to check
+ * Return: return true or false as output
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
+
+
