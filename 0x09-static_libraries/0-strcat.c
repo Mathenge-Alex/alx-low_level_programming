664b31777f0c7fd5bec71066fcaeba60d13565ca
Mathenge-Alex <arxmaina@gmail.com>
2024-01-13 04:10:03 +0300

    C libraries

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strcat - empty function
+ * @dest: Parameter
+ * @src: Parameter
+ * Return: returns empty
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
