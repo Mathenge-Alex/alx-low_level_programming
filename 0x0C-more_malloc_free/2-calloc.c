e3fb67d99ca907d374dddcfc220babe776ee6552
Mathenge-Alex <arxmaina@gmail.com>
2024-01-19 04:48:03 +0300

    memory set and calculator

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * *_memset - function fills memory
+ * with a constant byte
+ * @s: a pointer to put the constant
+ * @b: a constant
+ * @n: maximum bytes to use
+ * Return: returns s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *pointer = s;
+
+while (n--)
+	*s++ = b;
+
+return (pointer);
+}
+
+/**
+ * *_calloc - function allocates memory
+ * using malloc for an array,
+ * @nmemb: the array length
+ * @size: the size of each element
+ * Return: returns a pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *mem;
+
+if (size == 0 || nmemb == 0)
+	return (NULL);
+mem = malloc(sizeof(int) * nmemb);
+
+if (m == 0)
+	return (NULL);
+
+_memset(mem, 0, nmemb * sizeof(int));
+
+return (mem);
+}
