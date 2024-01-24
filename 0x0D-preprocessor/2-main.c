050abe33445a148d40fe2eb2d5d270596781584a
Mathenge-Alex <arxmaina@gmail.com>
2024-01-24 05:39:50 +0300

    main.c Precprocessors

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function prints the name
+ * of the file that was compiled
+ * Return: returns an int
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
