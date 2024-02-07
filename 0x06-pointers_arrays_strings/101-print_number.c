4c6686588fe8abda5a2b7ccd925224f15ff21ad8
Mathenge-Alex <arxmaina@gmail.com>
2024-02-07 14:50:48 +0300

    Printing a number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,38 @@
-#include "main.h"
-/**
-* print_number - Function prints numbers chars
-* @n: Integer parameter
-**/
+#include <unistd.h>
 
+int _putchar(char c);
+
+/**
+ * print_number - Prints an integer using recursion
+ * @n: The integer to print
+ *
+ * Description: Recursively divides the number to print each digit
+ */
 void print_number(int n)
 {
-		unsigned int size, factor, number;
+	if (n < 0)
+	{
+		_putchar('-'); /* Handle negative numbers */
+		if (n < -9)
+			print_number(-(n / 10));
+		_putchar('0' - (n % 10));
+	}
+	else
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + (n % 10));
+	}
+}
 
-		if (n < 0)
-		{
-				_putchar(45);
-				size = -n;
-		}
-		else
-		{
-				size = n;
-		}
-
-		factor = size;
-		number = 1;
-
-		while (factor > 9)
-		{
-				factor /= 10;
-				number *= 10;
-		}
-
-		for (; number >= 1; number /= 10)
-		{
-				_putchar(((size / number) % 10) + 48);
-		}
+/**
+ * _putchar - Writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
