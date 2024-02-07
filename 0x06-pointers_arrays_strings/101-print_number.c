9b9ff9faae77e0a4497744da814175b25ab05dd2
Mathenge-Alex <arxmaina@gmail.com>
2024-02-07 05:23:56 +0300

    Edit number chars function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,25 +1,35 @@
 #include "main.h"
 /**
-  * print_number - Function prints numbers chars
-  * @n: Integer parameter
-  * Return: Returns 0
- **/
+* print_number - Function prints numbers chars
+* @n: Integer parameter
+* Return: Returns 0
+**/
 
 void print_number(int n)
 {
-unsigned int n1;
+		unsigned int size, num, factor;
 
-n1 = n;
+		if (n < 0)
+		{
+				_putchar(45);
+				size = -n;
+		}
+		else
+		{
+				size = n;
+		}
 
-if (n < 0)
-{
-_putchar('-');
-n1 = -n;
-}
+		factor = size;
+		num = 1;
 
-if (n1 / 10 != 0)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+		while (factor > 9)
+		{
+				factor /= 10;
+				num *= 10;
+		}
+
+		for (; num >= 1; num /= 10)
+		{
+				_putchar(((size / num) % 10) + 48);
+		}
 }
