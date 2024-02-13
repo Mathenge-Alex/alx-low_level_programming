985145efa1c22432e23e222eb8a62ef5b18b3522
Mathenge-Alex <arxmaina@gmail.com>
2024-02-13 06:46:04 +0300

    File Input and Output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _strlen - Function finds string Length
+  * _strlen - String Length
   * @s: input char
   * Return: Returns length of a string
 **/
@@ -15,32 +15,32 @@ int _strlen(char *s)
 	return (j);
 }
 /**
-* create_text_file - Function creates a text_file
-* @text_filename: The text_file
-* @text_content: text_File content
+* create_file - Function creates a file
+* @filename: The file
+* @text_content: File content
 * Return: Returns 1 on success, or -1 on failure
 */
-int create_text_file(const char *text_filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	ssize_t letters_num;
-	int text_file;
+	int file;
 
-	if (!text_filename)
+	if (!filename)
 		return (1);
-	text_file = open(text_filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (text_file == -1)
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
 	{
 		return (-1);
 	}
 	if (text_content)
 	{
-		letters_num = write(text_file, text_content, _strlen(text_content));
+		letters_num = write(file, text_content, _strlen(text_content));
 		if (letters_num == -1)
 		{
-			close(text_file);
+			close(file);
 			return (-1);
 		}
 	}
-	close(text_file);
+	close(file);
 	return (1);
 }
