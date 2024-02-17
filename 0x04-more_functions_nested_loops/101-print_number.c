64429d1679669a830eaf461d466c609e518aef8e
Mathenge-Alex <arxmaina@gmail.com>
2024-02-17 12:48:23 +0300

    Functions and Nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,30 @@
 */
 void print_number(int n)
 {
+	int divisor = 1;
+
+	int is_negtv = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
+		is_negtv = 1;
 	}
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
 
-	_putchar(n % 10 + '0');
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
+
+	if (is_negtv == 0 && n == 0)
+	{
+		_putchar('0');
+	}
 }
