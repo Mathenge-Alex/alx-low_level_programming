6814b0ea3ba220062911fff9a55d41f2ab37100e
Mathenge-Alex <arxmaina@gmail.com>
2024-02-17 11:35:04 +0300

    Functions and Nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_triangle - Function prints an upright triangle of a given size.
+* @size: The triangle's size
+* Return - Returns nothing
+*/
+void print_triangle(int size)
+{
+	int j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (j = 0; j < size; j++)
+	{
+		for (k = 0; k <= j; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
