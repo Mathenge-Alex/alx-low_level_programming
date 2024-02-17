64ac296547cfaa5d9e910eb15da3accc05f169b8
Mathenge-Alex <arxmaina@gmail.com>
2024-02-17 11:29:13 +0300

    Functions and Nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Function's entry point
+*
+* Return: Returns always 0
+*/
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+			printf("FizzBuzz");
+		else if (j % 3 == 0)
+			printf("Fizz");
+		else if (j % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", j);
+
+		if (j < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
